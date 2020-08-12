a27c804116ed136a4b5ba2379a773485daac2b72
Pieter van der Heijden <p.van.der.heijden@calvi-insight.com>
2020-08-12 11:12:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Assign reviewers to pull requests action
+
+Use this action to assign reviewers to pull requests
+
+Assigns team members as reviewer, and sets assignee to current user 
+
+## Example usage
+
+```yaml
+- uses: calvi-business-software/assign-reviewers-to-pull-requests@v1
+```
+
---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
+  // TODO: add implementation
+
+} catch (error) {
+  core.setFailed(error.message);
+}
