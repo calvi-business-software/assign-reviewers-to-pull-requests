7d97b88e3453ccf3f07d6c245f3b3dbf50f76667
Pieter van der Heijden <p.van.der.heijden@calvi-insight.com>
2020-08-12 16:37:42 +0200

    Wrapped await in async function

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-  // TODO: add implementation
-
-  // pull_request > user > id / login
-
-  const token = core.getInput('ghToken');
-  const octokit = github.getOctokit(token);
-
-  console.log(token);
-
-  var teams = await octokit.teams.listForAuthenticatedUser();
-
-  console.log(teams);
-
-  foreach (team in teams)
-  {
-    console.log(team);
+async function run() {
+  try {
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  
+    // TODO: add implementation
+  
+    // pull_request > user > id / login
+  
+    const token = core.getInput('ghToken');
+    const octokit = github.getOctokit(token);
+  
+    console.log(token);
+  
+    var teams = await octokit.teams.listForAuthenticatedUser();
+  
+    console.log(teams);
+  
+    foreach (team in teams)
+    {
+      console.log(team);
+    }
+  
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
