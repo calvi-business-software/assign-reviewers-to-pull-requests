e81318c6921b5149991b5bd25cfe4f4ac0f60348
Pieter van der Heijden <p.van.der.heijden@calvi-insight.com>
2020-08-13 08:36:17 +0200

    Added business logic

--- index.js
+++ index.js
@@ -3,26 +3,71 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-  
-    // TODO: add implementation
-  
-    // pull_request > user > id / login
-  
+    // Init octokit
     const token = core.getInput('ghToken');
     const octokit = github.getOctokit(token);
-  
-    console.log(token);
-  
-    var teams = await octokit.teams.listForAuthenticatedUser();
-  
-    console.log(teams);
-  
-    foreach (team in teams)
-    {
-      console.log(team);
+
+    // Get info from payload
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    var login = payload.sender.login;
+    var org = payload.organization.login;
+    var repo = payload.repository.name;
+    var pullNumber = payload.pull_request.number;
+
+    // Debug statements
+    console.log(`login: '${login}'`)
+    console.log(`org: '${org}'`)
+    console.log(`repo: '${repo}'`)
+    console.log(`pullNumber: '${pullNumber}'`)
+
+    // Get teams for org
+    var orgTeams = await octokit.teams.list({
+      org: org
+    });
+    var orgTeams = orgTeams.data;
+
+    // Get teams and teamMembers for user
+    var teams = []
+    var teamMembers = [];
+    for (var index in orgTeams) {
+      var team = orgTeams[index];
+
+      var members = await octokit.teams.listMembersInOrg({
+        org: org,
+        team_slug: team.slug
+      });
+      var members = members.data;
+      
+      if (members.some(member => member.login == login)) {
+        teams = [...teams, team];
+        teamMembers = [...teamMembers, ...members.filter(member => member.login != login)];
+      }
     }
+
+    // Define reviewers
+    var reviewers = [... new Set(teamMembers.map(member => member.login))];
+    var teamReviewers = teams.map(team => team.slug);
+
+    // Debug statements
+    console.log(`reviewers: '${reviewers}'`);
+    console.log(`teamReviewers: '${teamReviewers}'`);
+
+    // Request reviewers
+    await octokit.pulls.requestReviewers({
+      owner: org,
+      repo: repo,
+      pull_number: pullNumber,
+      reviewers: reviewers,
+      team_reviewers: teamReviewers
+    });
+
+    // Add assignee
+    await octokit.issues.addAssignees({
+      owner: org,
+      repo: repo,
+      issue_number: pullNumber,
+      assignees: login
+    });    
   
   } catch (error) {
     core.setFailed(error.message);
